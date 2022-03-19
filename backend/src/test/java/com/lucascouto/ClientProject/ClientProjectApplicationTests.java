4f953d7889b45878d78a6bc2039dbea534bc7998
Lucas Couto <loc.unb@gmail.com>
2022-03-19 02:18:09 -0300

    Project Created

---
+++ backend/src/test/java/com/lucascouto/ClientProject/ClientProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucascouto.ClientProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
