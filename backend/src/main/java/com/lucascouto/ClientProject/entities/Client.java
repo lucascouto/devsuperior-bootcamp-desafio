9ce7e4065c0329262a59f7d704bee151cf331329
Lucas Couto <loc.unb@gmail.com>
2022-03-19 02:28:14 -0300

    Map entity to table

--- backend/src/main/java/com/lucascouto/ClientProject/entities/Client.java
+++ backend/src/main/java/com/lucascouto/ClientProject/entities/Client.java
@@ -2,13 +2,27 @@ package com.lucascouto.ClientProject.entities;
 
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
 public class Client {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String cpf;
 	private Double income;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
+	
 	private Integer children;
 	
 	public Client() {}
